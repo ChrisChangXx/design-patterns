aca66cf04c3c1b7bd0ef8f6fdb83e347e94705fb
zhanghui <zhanghui@chargertech.cn>
2022-03-08 18:05:00 +0800

    新加prac项目dps

---
+++ src/main/java/com/chris/dp/factory/abs/AbstractFactoryTest.java
@@ -0,0 +1,20 @@
+package com.chris.dp.factory.abs;
+
+import com.chris.dp.factory.abs.store.NyPizzaStore;
+import com.chris.dp.factory.abs.store.PizzaStore;
+
+/**
+ * 抽象工厂测试
+ *
+ * @author zhx
+ * @创建人 zhanghui
+ * @创建时间 2021/6/6
+ * @描述
+ * @date 2021/06/06
+ */
+public class AbstractFactoryTest {
+    public static void main(String[] args) {
+        PizzaStore pizzaStore = new NyPizzaStore();
+        pizzaStore.orderPizza("cheese");
+    }
+}
